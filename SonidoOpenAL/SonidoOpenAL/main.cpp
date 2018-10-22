3fc9aa3d490f7b82ba475cd3d4202c701bc7ab82
Manuel Hernández <manherna@ucm.es>
2018-10-22 19:49:44 +0300

    Añadido el trabajo en laboratorio  del 17-10

---
+++ SonidoOpenAL/SonidoOpenAL/main.cpp
@@ -0,0 +1,32 @@
+#include <al.h>
+#include <alc.h>
+#include <alut.h>
+#include <iostream>
+
+int main()
+{	
+	alutInit(0, NULL);
+	alGetError();
+	ALfloat listenerOri[] = { 0.0f, 0.0f, 1.0f, 0.0f, 1.0f, 0.0f };
+
+	alListener3f(AL_POSITION, 0, 0, 1.0f);
+	// check for errors
+	alListener3f(AL_VELOCITY, 0, 0, 0);
+	// check for errors
+
+	// Esto es para cambiar la orientación del oyente.
+	//alListenerfv(AL_ORIENTATION, listenerOri);
+	// check for errors
+
+	ALuint buffer = alutCreateBufferFromFile("../SonidoOpenAL/Sonidos/motores/1.wav");	if (alutGetError() != AL_NO_ERROR) {		std::cout << alutGetError() << std::endl;		std::getchar();	}	ALuint source;	alGenSources(1, &source);	/*	// Para cambiar el pitch
+	// posicion y velociddad: vectores en coordenadas cartesianas
+	ALfloat SourcePos[] = { 0.0, 0.0, 0.0 };
+	ALfloat SourceVel[] = { 0.0, 0.0, 0.0 };
+	alSourcefv(Source, AL_POSITION, SourcePos);
+	alSourcefv(Source, AL_VELOCITY, SourceVel);
+	alSourcef (Source, AL_PITCH, 1.0 ); // frec de reproduccion
+	alSourcef (Source, AL_GAIN, 1.0 ); // ganancia de la fuente
+	alSourcei (Source, AL_LOOPING, true); // para repr. ciclica	*/	alSourcei(source, AL_BUFFER, buffer);	alSourcePlay(source);		alutSleep(3);	alutExit();
+
+	return 0;
+}
