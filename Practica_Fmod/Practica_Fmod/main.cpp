f24523130b758091f846741d7f0e544284d59dc3
Manuel Hernández <manherna@ucm.es>
2018-11-05 18:18:05 +0200

    Añadido el piano to wapo

--- Practica_Fmod/Practica_Fmod/main.cpp
+++ Practica_Fmod/Practica_Fmod/main.cpp
@@ -2,7 +2,8 @@
 #include <fmod_errors.h>
 #include <iostream>
 #include <thread>
-
+#include <cstdio>
+#include <conio.h>
 
 using namespace FMOD;
 System * syst;
@@ -11,7 +12,7 @@ void ERRCHECK(FMOD_RESULT result)
 {
 	if (result != FMOD_OK) {
 		std::cout << FMOD_ErrorString(result) << std::endl;
-		exit(-1);
+		//exit(-1);
 	}
 
 }
@@ -19,23 +20,24 @@ void ERRCHECK(FMOD_RESULT result)
 class PotatoSOUND
 {	
 public:
-
+	PotatoSOUND(){}
 	PotatoSOUND(const char * path) 
 	{
 		FMOD_RESULT res;
 		res = syst->createSound(path, FMOD_DEFAULT, 0, &_sound);
 		ERRCHECK(res);
-	
-
+		res = syst->playSound(_sound, 0, true, &_channel);
+		ERRCHECK(res);
 	}
 	~PotatoSOUND()
 	{
 		_sound->release();
-		delete _channel;
 	}
 	void PlaySound()
 	{
 		FMOD_RESULT res = syst->playSound(_sound, 0, false, &_channel);
+		ERRCHECK(res);
+
 	}
 	void togglePause()
 	{	
@@ -43,6 +45,12 @@ public:
 		_channel->getPaused(&paused);
 		_channel->setPaused(!paused);	
 
+	}
+	void setPitch(float i)
+	{
+		FMOD_RESULT res;
+		res = _channel->setPitch(i);
+		ERRCHECK(res);
 	}
 	void fadeIn() {
 		unsigned long long parentclock;
@@ -66,7 +74,14 @@ private:
 	
 };
 
-
+template <class T>
+int getIndex(T * arr, T find, int size) {
+	int z = -1;
+	for (int i = 0; i < size; i++) {
+		if (arr[i] == find)return i;
+	}
+	return z;
+}
 
 
 int main() {
@@ -79,15 +94,39 @@ int main() {
 		res = syst->init(128, FMOD_INIT_NORMAL, 0);
 		ERRCHECK(res);
 	}
-	PotatoSOUND a("../muestras/talking.wav");
-	a.PlaySound();
-	a.fadeOut();
+	char * notas = "cdefgab";
+	char * teclas = "zxcvbnm,";
+	short int frec[8] = {0,2,4,5,7,9,11,12};
+	float pitch [8];
+	PotatoSOUND * sounds [8];
 
-	while (true)
-		syst->update();
+	for (int i = 0; i < 8; i++) {
+		pitch[i] = std::pow(2, (frec[i] / 12.0f));
+		std::cout << pitch[i] << std::endl;
+		sounds[i] = new PotatoSOUND("../muestras/piano.ogg");
+	}
 
-
-
+	int i = 0;
+	while (true) {
+		if(_kbhit()){
+			char a = _getch();
+			if (a == 'q')break;
+			else {
+				int i = getIndex(teclas, a, 8);
+				if (i == -1)continue;
+				else {
+					std::cout << notas[i] << " pitch: " << pitch[i] << std::endl;
+					sounds[i]->PlaySound();
+					sounds[i]->setPitch(pitch[i]);
+				}
+			}
+		}
+		syst->update();	
+	}
+	std::cout << "Closing.." << std::endl;
+	for (int i = 0; i < 8; i++) {
+		delete sounds[i];
+	}
 	FMOD_RESULT res = syst->release();
 	ERRCHECK(res);
 	return 0;
