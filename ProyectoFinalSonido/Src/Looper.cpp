54e2d213f6f387252be08fc3973392396de6d4e7
mjcguada <mjcguada@gmail.com>
2018-12-08 13:43:33 +0100

    carga imagenes fallando

--- ProyectoFinalSonido/Src/Looper.cpp
+++ ProyectoFinalSonido/Src/Looper.cpp
@@ -41,6 +41,10 @@ bool Looper::init()
 	_activeChannel = -1;
 
 	emptySound = loadMedia("../Images/pistaFull.png");
+	if (emptySound == NULL)
+	{
+		printf("Failed to load texture image!\n");		
+	}
 	
 	//fullSound = loadMedia("../Images/pistaFull.png");
 		
@@ -69,15 +73,29 @@ bool Looper::init()
 
 SDL_Texture* Looper::loadMedia(std::string path)
 {
+	//The final texture
+	SDL_Texture* newTexture = NULL;
+
 	//Load image at specified path
 	SDL_Surface* loadedSurface = IMG_Load(path.c_str());
-
 	if (loadedSurface == NULL)
 	{
 		printf("Unable to load image %s! SDL_image Error: %s\n", path.c_str(), IMG_GetError());
-	}	
+	}
+	else
+	{
+		//Create texture from surface pixels
+		newTexture = SDL_CreateTextureFromSurface(renderer, loadedSurface);
+		if (newTexture == NULL)
+		{
+			printf("Unable to create texture from %s! SDL Error: %s\n", path.c_str(), SDL_GetError());
+		}
 
-	return SDL_CreateTextureFromSurface(renderer, loadedSurface);
+		//Get rid of old loaded surface
+		SDL_FreeSurface(loadedSurface);
+	}
+
+	return newTexture;
 }
 
 void Looper::release() {
